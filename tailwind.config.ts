ed9a837ebd6fe94f2a9648dccf3f32ed8e108bf2
dorisunandi <dorisunandi@gmail.com>
2024-11-29 14:01:22 +0700

    commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+
 
 export default {
   content: [
@@ -6,13 +6,57 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix:"",
   theme: {
+    
     extend: {
+      fontFamily:{
+        primay:"var(--font-jetbrainsMono)",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#1c1c22",
+        accent:{
+          DEFAULT: "#08FF99",
+          hover:"#00e187",
+        },
+      },
+      keyframes:{
+        'accordion-down':{
+          from:{height:"0"},
+          to:{height:"var(--radix-accordion-content-height)"},
+        },
+        'accordion-up':{
+          from:{height:"var(--radix-accordion-content-height)"},
+          to:{height:"0"},
+
+          },
+
+        },
+    animation:{
+      'accordion-down':"accordion-down 0.2s ease-out",
+      'accordion-up':"accrdion-up 0.2s ease-out"
+    },
+      },
+    
+     
+    container:{
+      center:true,
+      padding: '15px',
+      screens:{
+        sm:'648px',
+        md:'768px',
+        lg:'968px',
+        xl:'1200px',
+      },
+     
+   
+
       },
     },
-  },
-  plugins: [],
-} satisfies Config;
+    
+    
+    plugins : [require("tailwindcss-animate")],
+    }
+
